db49eff1bceb79dd986844c0e17e2c86e215f730
Nikolay <kolik_2604@mail.ru>
2023-05-30 10:44:53 +0300

    second commit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,16 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+
+    const data = [
+        {name: 'Nikolas Fon Peroff', salary:625, increase:true, id:1},
+        {name: 'Anton Killer', salary:12500, increase:true, id:2},
+        {name: 'Sir-gay Sherstyouk', salary:625, increase:false, id:3},
+        {name: 'Artyom Enlightened', salary:500, increase:false, id:4},
+        {name: 'Dmitriy Aching', salary:1000, increase:true, id:5}
+    ];
+
+
     return (
 <div className='app'>
     <AppInfo/>
@@ -14,7 +24,7 @@ function App() {
             <SearchPanel/> 
             <AppFilter/>
         </div>
-    <EmployeesList/>
+    <EmployeesList data={data}/>
     <EmployeesAddForm/>
 </div>
     );
