a42618b3b673a450781119225983d64b6a596827
Nikolay <kolik_2604@mail.ru>
2023-06-02 15:10:38 +0300

    task 2

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,28 +8,47 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data : [
+                {name: 'Nikolas Fon Peroff', salary:625, increase:true, id:1},
+                {name: 'Anton Killer', salary:12500, increase:true, id:2},
+                {name: 'Sir-gay Sherstyouk', salary:625, increase:false, id:3},
+                {name: 'Artyom Enlightened', salary:500, increase:false, id:4},
+                {name: 'Dmitriy SoloSnev', salary:1000, increase:true, id:5}
+            ]
+        }
+    }
+    
+deleteItem = (id) => {
+    this.setState (({data})=> {
+return {data:data.filter(item=> item.id !==id)}
+    })
+}
+addEmployee = (e) => {
+ e.preventDefault();
 
-    const data = [
-        {name: 'Nikolas Fon Peroff', salary:625, increase:true, id:1},
-        {name: 'Anton Killer', salary:12500, increase:true, id:2},
-        {name: 'Sir-gay Sherstyouk', salary:625, increase:false, id:3},
-        {name: 'Artyom Enlightened', salary:500, increase:false, id:4},
-        {name: 'Dmitriy Aching', salary:1000, increase:true, id:5}
-    ];
+}
 
 
+
+render() {
     return (
-<div className='app'>
-    <AppInfo/>
-        <div className="search-panel">
-            <SearchPanel/> 
-            <AppFilter/>
+        <div className='app'>
+            <AppInfo/>
+                <div className="search-panel">
+                    <SearchPanel/> 
+                    <AppFilter/>
+                </div>
+            <EmployeesList data={this.state.data}
+            onDelete={this.deleteItem}/>
+            <EmployeesAddForm/>
         </div>
-    <EmployeesList data={data}/>
-    <EmployeesAddForm/>
-</div>
-    );
+            );
+}
+
 }
 
 export default App;
--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,23 +1,57 @@
+import { Component } from 'react';
+
 import './employees-add-form.css'
 
+class EmployeesAddForm extends Component {
+        constructor(props) {
+                super(props);
+                this.state = {
+                        name:'',
+                        salary:0
+                }
+        }
+        onValueChange = (e) => {
+        this.setState({
+        [e.target.name]:e.target.value
+})
+        }
+        addEmployee = (e,{data}) => {
+                e.preventDefault();
+                this.setState (({data})=> {
+                        return {}
+                
+        })  
+        }
 
-const EmployeesAddForm = () => {
-    return (
-       <div className="app-add-form">
-        <h3>Добавьте нового идиота</h3>
-        <form className='add-form d-flex'>
-            <input type='text'
-                    className='form-control new-post-label'
-                    placeholder='Как же его зовут?'/>
-            <input type='number' 
-                    className='form-control new-post-label' 
-                    placeholder='З/П в $?'/>
 
-            <button type='submit'
-                    className='btn btn-outline-light'>Добавить</button>
-        </form>
-       </div>  
-    )
+render () {
+        const {name, salary,addEmployee} = this.state;
+        return (
+                <div className="app-add-form">
+                 <h3>Добавьте нового идиота</h3>
+                 <form className='add-form d-flex'>
+                     <input type='text'
+                             className='form-control new-post-label'
+                             placeholder='Как же его зовут?'
+                             name = 'name'
+                             value={name} 
+                             
+                             onChange={this.onValueChange}
+                              />
+                     <input type='number' 
+                             className='form-control new-post-label' 
+                             placeholder='З/П в $?'
+                             name='salary' 
+                             value={salary}
+                             onChange={this.onValueChange}
+                             />
+                     <button type='submit'
+                             className='btn btn-outline-light'
+                             onClick={addEmployee}>Добавить</button>
+                 </form>
+                </div>  
+             )
+}
 }
 
 export default EmployeesAddForm;
